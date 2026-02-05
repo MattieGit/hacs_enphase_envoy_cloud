029c2b46eeae108108a849a34c8867ca09c06c76
Mattie <6250046+MattieGit@users.noreply.github.com>
2026-02-05 13:15:13 +0100

    chore: bump version to 1.8.0 for UX improvements

--- custom_components/enphase_envoy_cloud_control/const.py
+++ custom_components/enphase_envoy_cloud_control/const.py
@@ -4,7 +4,7 @@ import logging
 
 DOMAIN = "enphase_envoy_cloud_control"
 NAME = "Enphase Envoy Cloud Control"
-VERSION = "1.7.0"
+VERSION = "1.8.0"
 DEVICE_KIND_BATTERY = "battery"
 DEVICE_KIND_SCHEDULE_EDITOR = "schedule_editor"
 
